60903d8efc045bb3d728df94bc381b709f864cac
rahulroy9202 <rahulroy9202@gmail.com>
2015-04-29 16:32:25 +0530

    testing openshift compatibllity with minimal server

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
