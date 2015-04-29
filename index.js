98a6e7d134ec023335573f7945001b5441e2b6b3
rahulroy9202 <rahulroy9202@gmail.com>
2015-04-29 16:42:19 +0530

    fixing repo

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var http = require("http");
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
+  response.write("Hello World Dadum Tush");
   response.end();
 }).listen(process.env.OPENSHIFT_NODEJS_PORT || 8080,process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+
+//noth
